5f9d3d06617cd570fd4b7506688c97fb2b4235d0
xtr <99972602+arafatrahman862@users.noreply.github.com>
2025-05-22 03:54:08 +0600

    data deletion command

--- foreignKey.sql
+++ foreignKey.sql
@@ -6,5 +6,51 @@ create Table "user" (
 create Table post (
     id serial primary key,
     title text not null,
-    user_id int REFERENCES "user" (id) 
+    user_id int REFERENCES "user" (id) on delete cascade
 );
+create Table post (
+    id serial primary key,
+    title text not null,
+    user_id int REFERENCES "user" (id) on delete set null
+);
+create Table post (
+    id serial primary key,
+    title text not null,
+    user_id int REFERENCES "user" (id) on delete set DEFAULT DEFAULT 2
+);
+
+insert INTO "user" (username) VALUES
+    ('user1'),
+    ('user2'),
+    ('user3'),
+    ('user4'),
+    ('user5'),
+    ('user6'),
+    ('user7'),
+    ('user8'),
+    ('user9'),
+    ('user10');
+
+
+select * FROM "user";
+select * FROM post;
+
+insert INTO post (title, user_id) VALUES
+    ('post1', 1),
+    ('post2', 2),
+    ('post3', 3),
+    ('post4', 4),
+    ('post5', 5),
+    ('post6', 6),
+    ('post7', 7),
+    ('post8', 8),
+    ('post9', 9),
+    ('post10', 10);
+
+select * FROM post;
+
+drop Table post;
+drop Table "user";
+
+delete FROM "user" 
+    WHERE id = 4;
