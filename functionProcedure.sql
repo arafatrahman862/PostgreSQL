3c59e672e6ed6a65ad229acc3d13c37c9eecc93d
xtr <99972602+arafatrahman862@users.noreply.github.com>
2025-05-23 01:44:12 +0600

    trigger command added

--- functionProcedure.sql
+++ functionProcedure.sql
@@ -1,31 +1,63 @@
-select * FROM employees;
-create Function emp_count()
-returns INT
-language SQL
+
+
+SELECT * from employees;
+
+SELECT count(*) from employees;
+
+CREATE or replace Function emp_count()
+RETURNS void
+LANGUAGE SQL
 as
 $$
-select count(*) FROM employees;
-$$
-;
+    DELETE FROM employees WHERE employee_id = 30;
+$$;
 
-select emp_count();
-create Function delete_emp()
-returns void
-language SQL
+CREATE or replace Function delete_emp()
+RETURNS void
+LANGUAGE SQL
 as
 $$
-delete FROM employees WHERE employee_id = 30;
-$$
-;
+    DELETE FROM employees WHERE employee_id = 30;
+$$;
 
-select delete_emp();
-create Function delete_emp_by_id(p_emp_id INT)
-returns void
-language SQL
+SELECT delete_emp ();
+
+CREATE or replace Function delete_emp_by_id(p_emp_id int)
+RETURNS void
+LANGUAGE SQL
 as
 $$
-delete FROM employees WHERE employee_id = p_emp_id;
-$$
-;
+    DELETE FROM employees WHERE employee_id = p_emp_id;
+$$;
 
-select delete_emp_by_id(29);
+SELECT delete_emp_by_id (29)
+
+CREATE Procedure remove_emp_var()
+LANGUAGE plpgsql
+AS
+$$
+    DECLARE
+    test_var int;
+    BEGIN
+        SELECT employee_id INTO test_var from employees WHERE employee_id = 26;
+        DELETE FROM employees WHERE employee_id = test_var;
+    END
+$$;
+
+call remove_emp_var ();
+
+CREATE Procedure remove_emp_by_id(p_emp_id int)
+LANGUAGE plpgsql
+AS
+$$
+    DECLARE
+    test_var int;
+    BEGIN
+        SELECT employee_id INTO test_var from employees WHERE employee_id = p_emp_id;
+        DELETE FROM employees WHERE employee_id = test_var;
+
+        RAISE NOTICE 'Employee removed successfully!';
+    END
+$$;
+
+CALL remove_emp_by_id (25)
